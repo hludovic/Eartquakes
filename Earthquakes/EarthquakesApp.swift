416673639c31e233cd6e8735ae6350433190e3d8
Ludovic HENRY <58089826+hludovic@users.noreply.github.com>
2024-01-23 22:22:20 -0400

    Initial Commit

---
+++ Earthquakes/EarthquakesApp.swift
@@ -0,0 +1,17 @@
+//
+//  EarthquakesApp.swift
+//  Earthquakes
+//
+//  Created by Ludovic HENRY on 23/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct EarthquakesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
