bec12d36c1ce7288c42b65406a5a72872356fab6
Ludovic HENRY <58089826+hludovic@users.noreply.github.com>
2024-02-26 17:24:17 -0400

    DecodedEarthquakes in a separate file

---
+++ Earthquakes/Model/DecodedEarthquakes.swift
@@ -0,0 +1,82 @@
+//
+//  DecodedEarthquakes.swift
+//  Earthquakes
+//
+//  Created by Ludovic HENRY on 26/02/2024.
+//
+
+import Foundation
+
+extension EarthquakeViewModel {
+
+    struct DecodedEarthquakes: Decodable {
+        let count: Int
+        let title: String
+        let earthquakes: [Earthquake]
+
+        init(from decoder: Decoder) throws {
+            var result: [Earthquake] = []
+
+            let rawEarthquake = try RawResponse(from: decoder)
+            let features = rawEarthquake.features
+
+            for feature in features {
+                let earthquake = Earthquake(
+                    id: feature.id,
+                    magnitude: feature.properties.mag,
+                    place: feature.properties.place,
+                    time: feature.properties.time,
+                    updated: feature.properties.updated,
+                    url: feature.properties.url,
+                    alert: feature.properties.alert,
+                    tsunami: feature.properties.tsunami,
+                    sig: feature.properties.sig,
+                    type: feature.properties.type,
+                    title: feature.properties.title,
+                    longitude: feature.geometry.coordinates[0],
+                    latitude: feature.geometry.coordinates[1])
+                result.append(earthquake)
+            }
+            earthquakes = result
+            count = rawEarthquake.metadata.count
+            title = rawEarthquake.metadata.title
+        }
+    }
+
+    private struct RawResponse: Decodable {
+        let type: String
+        let metadata: Metadata
+        let features: [Feature]
+
+        struct Metadata: Decodable {
+            let title: String
+            let status: Int
+            let count: Int
+        }
+
+        struct Feature: Decodable {
+            let id: String
+            let properties: Propertie
+            let geometry: Geometry
+
+            struct Propertie: Decodable {
+                let mag: Float
+                let place: String
+                let time: Date
+                let updated: Date
+                let url: String
+                let alert: String?
+                let tsunami: Int
+                let sig: Int
+                let type: String
+                let title: String
+            }
+
+            struct Geometry: Decodable {
+                let type: String
+                let coordinates: [Double]
+            }
+        }
+    }
+}
+
--- Earthquakes/ViewModel/EarthquakeViewModel.swift
+++ Earthquakes/ViewModel/EarthquakeViewModel.swift
@@ -18,78 +18,14 @@ class EarthquakeViewModel: ObservableObject {
         let decodedEarthquakes: DecodedEarthquakes = try await httpClient.fetch(url: url, dateDecodingStrategy: .millisecondsSince1970)
         earthquakes = decodedEarthquakes.earthquakes
     }
+}
 
-    private func getGeoJSONFeedUrl(since period: ApiJsonFeeds.Period, strength: ApiJsonFeeds.Strength) -> URL? {
+private extension EarthquakeViewModel {
+
+    func getGeoJSONFeedUrl(since period: ApiJsonFeeds.Period, strength: ApiJsonFeeds.Strength) -> URL? {
         let parameter = ApiJsonFeeds.parameters(period: period, strength: strength)
         let urlString: String = ApiJsonFeeds.baseURL + ApiJsonFeeds.endpoint + "/" + parameter + "." + ApiJsonFeeds.FileFormat.json
         return URL(string: urlString)
     }
 }
-
-private struct DecodedEarthquakes: Decodable {
-    var earthquakes: [Earthquake]
-
-    init(from decoder: Decoder) throws {
-        var result: [Earthquake] = []
-
-        let rawEarthquake = try RawResponse(from: decoder)
-        let features = rawEarthquake.features
-
-        for feature in features {
-            let earthquake = Earthquake(
-                id: feature.id,
-                magnitude: feature.properties.mag,
-                place: feature.properties.place,
-                time: feature.properties.time,
-                updated: feature.properties.updated,
-                url: feature.properties.url,
-                alert: feature.properties.alert,
-                tsunami: feature.properties.tsunami,
-                sig: feature.properties.sig,
-                type: feature.properties.type,
-                title: feature.properties.title,
-                longitude: feature.geometry.coordinates[0],
-                latitude: feature.geometry.coordinates[1])
-            result.append(earthquake)
-        }
-        earthquakes = result
-    }
-}
-
-private struct RawResponse: Decodable {
-    let type: String
-    let metadata: Metadata
-    let features: [Feature]
-}
-
-private struct Metadata: Decodable {
-    let title: String
-    let status: Int
-    let count: Int
-}
-
-private struct Propertie: Decodable {
-    let mag: Float
-    let place: String
-    let time: Date
-    let updated: Date
-    let url: String
-    let alert: String?
-    let tsunami: Int
-    let sig: Int
-    let type: String
-    let title: String
-}
-
-private struct Feature: Decodable {
-    let id: String
-    let properties: Propertie
-    let geometry: Geometry
-}
-
-private struct Geometry: Decodable {
-    let type: String
-    let coordinates: [Double]
-}
-
 
