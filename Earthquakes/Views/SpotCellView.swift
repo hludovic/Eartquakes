4b118eaf5e1da909fe5b019dde69965007cada6a
Ludovic HENRY <58089826+hludovic@users.noreply.github.com>
2024-03-11 21:21:20 -0400

    Test a CellView

--- Earthquakes/Views/SpotCellView.swift
+++ Earthquakes/Views/SpotCellView.swift
@@ -11,16 +11,21 @@ struct SpotCellView: View {
     @State var content: Earthquake
 
     var body: some View {
-        Text("M - " + String(format: "%.1f", 4.3))
-            .frame(width: 90, height: 90, alignment: .center)
-            .background { Color(.blue).clipShape(Circle()) }
-            .background {
-                Color(.red)
+        HStack {
+            Text("M - " + String(format: "%.1f", 4.3))
+                .frame(width: 90, height: 90, alignment: .center)
+                .background { Color(.blue).clipShape(Circle()) }
+//                .background { Color(.red) }
+            VStack {
+                Text(content.place)
+                Text(String(format: "%.1f", content.depth) + " Km")
             }
-            .frame(maxWidth: .infinity)
-            .border(Color.blue)
-        Text(content.place)
-        Text(String(format: "%.1f", content.depth) + " Km")
+            Spacer()
+        }
+        .frame(height: 90)
+        .frame(maxWidth: .infinity)
+        .border(Color.red)
+        .padding()
     }
 }
 
