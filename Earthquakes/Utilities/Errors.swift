1930ed5e29cfd696a9e741dab9efda5c13cda74c
Ludovic HENRY <58089826+hludovic@users.noreply.github.com>
2024-07-04 13:19:55 -0400

    Errors give a description

--- Earthquakes/Utilities/Errors.swift
+++ Earthquakes/Utilities/Errors.swift
@@ -5,6 +5,34 @@
 //  Created by Ludovic HENRY on 17/02/2024.
 //
 
+import Foundation
+
 enum HttpError: Error {
-    case badURL, badResponse, errorDecodingData, invalidURL
+    case badResponse
+    case errorDecodingData
+    case invalidURL
+}
+
+extension HttpError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .badResponse:
+            "Bad Response"
+        case .errorDecodingData:
+            "Error Decoding Data"
+        case .invalidURL:
+            "Invalid URL"
+        }
+    }
+
+    var failureReason: String? {
+        switch self {
+        case .badResponse:
+            "An error occurred when receiving the data."
+        case .errorDecodingData:
+            "An error occurred while processing the data."
+        case .invalidURL:
+            "Unable to communicate with the server."
+        }
+    }
 }
